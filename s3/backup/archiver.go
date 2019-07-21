3dd5ca3aca6ca0dfb0622f5d11e36221ce022d94
markday1962 <mark@mark-day.me>
2019-07-21 09:24:38 +0100

    commit completed dirhash.go and monitor.go

--- s3/backup/archiver.go
+++ s3/backup/archiver.go
@@ -8,12 +8,17 @@ import (
 )
 
 type Archiver interface {
+	DestFmt() string
 	Archive(src, dest string) error
 }
 
 type zipper struct{}
 
 var ZIP Archiver = (*zipper)(nil)
+
+func (z *zipper) DestFmt() string {
+	return "%d.zip"
+}
 
 func (z *zipper) Archive(src, dest string) error {
 	//ensures the destination directory exists
