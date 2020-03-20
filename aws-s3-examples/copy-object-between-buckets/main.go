1333c3e82712423995e49b9ff80b1f07415c7650
Mark Day <mark.day@aistemos.com>
2020-03-20 16:09:57 +0000

    bumped version

--- aws-s3-examples/copy-object-between-buckets/main.go
+++ aws-s3-examples/copy-object-between-buckets/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"os"
 )
 
@@ -21,31 +18,5 @@ func main() {
 		exitErrorf("Source bucket, Object, Target bucket are required" +
 			"\nUsage: %s source_bucket object_name target_bucket", os.Args[0])
 	}
-	sourceb := os.Args[1]
-	object := os.Args[2]
-	targetb := os.Args[3]
-	source := sourceb + "/" + object
 
-	//Initialize the session that the SDK uses to load credentials from the shared credentials file ~/.aws/credentials
-	sess, err := session.NewSession(&aws.Config {Region: aws.String("eu-west-1")},)
-	svc := s3.New(sess)
-
-	_, err = svc.CopyObject(&s3.CopyObjectInput{
-		Bucket: aws.String(targetb),
-		CopySource: aws.String(source),
-		Key: aws.String(object),
-	})
-	if err != nil{
-		exitErrorf("Unable to copy object %q from %q to %q, v%\n", object, sourceb, targetb, err)
-	}
-
-	// Wait to see if the object has copied successfully
-	err = svc.WaitUntilObjectExists(&s3.HeadObjectInput{
-		Bucket: aws.String(targetb),
-		Key: aws.String(object),
-	})
-	if err != nil {
-		exitErrorf("Error occurred while waiting for object %q to copied from %q to %q, %v", object, sourceb, targetb, err)
-	}
-	fmt.Printf("Object %q successfully copied from bucket %q to bucket %q\n", object, sourceb, targetb)
 }
